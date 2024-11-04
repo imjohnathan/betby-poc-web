cff1091d4c2493141128f453da9c3ed0ce65e9c2
Johnathan <johnathan.liu@nextfortune.tw>
2024-11-04 17:20:30 +0800

    refactor(vite.config.ts): update build and env config
    Refactor the Vite configuration to include environment loading with `loadEnv` and update build options for better file naming conventions. Introduce `projectName` for dynamic file naming and remove unused library build settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,38 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path';
-
+const projectName = "betby-component";
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    define: {
+      'process.env': {
+        NODE_ENV: JSON.stringify(env.NODE_ENV)
+      },
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
         '@': resolve(__dirname, 'src'),
+      },
     },
-},
-  build: {
-    lib: {
-      entry: "./src/main.tsx",
-      name: "betby-component",
-      fileName: (format) => `betby-component.${format}.js`,
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: `${projectName}-entry.js`,
+          chunkFileNames: `${projectName}-[name]-[hash].js`,
+          assetFileNames: `${projectName}-[name]-[hash].[ext]`,
+        },
+      },
     },
-    target: "esnext",
-  },
+    // build: {
+    //   lib: {
+    //     entry: ["./index.html"],
+    //     name: "betby-component",
+    //     fileName: (format) => `betby-component.${format}.js`,
+    //   },
+    //   target: "esnext",
+    // },
+  }
 })
